146086d6a2c092422975664a1dabbb5b320d212f
banskt <bnrj.saikat@gmail.com>
2020-11-02 09:58:00 -0600

    First comparison of ridge regression

---
+++ _notebooks/glmnet_mle_ridge.R
@@ -0,0 +1,66 @@
+standardize = function(X) {
+  ones = rep(1, nrow(X))
+  Xoff = X - ones %*% t(colMeans(X))
+  scale = ones %*% t(colSums(Xoff*Xoff))
+  Xstd = Xoff / scale
+  return (Xstd)
+}
+
+ridge_data = function(n, p, sd){
+  X = matrix(rnorm(n*p), nrow=n, ncol = p)
+  btrue = rnorm(p)
+  sd = 10
+  X = standardize(X)
+  y = drop(X %*% btrue + sd*rnorm(n))
+  y = y - mean(y)
+  return (list(X = X, y = y))
+}
+
+ridge_ols = function(y, X, lmbda) {
+  XtX = crossprod(X)
+  Xty = drop(crossprod(X,y))
+  yty = drop(crossprod(y))
+  p = ncol(X)
+  V = XtX + diag(lmbda, p)
+  Vinv = chol2inv(chol(V))
+  bhat = drop(V %*% Xty)
+  return (list(coef = bhat, ypred = drop(X %*% bhat)))
+}
+
+ridge_glmnet = function(y, X, lmbda) {
+  glm_lmbda = 2 * lmbda / nrow(X)
+  fit = glmnet(X, y, alpha = 0, lambda = glm_lmbda, intercept = FALSE, standardize = FALSE)
+  b = coef(fit, s = glm_lmbda)  # extract coefficients at a single value of lambda
+  ypred = c(predict(fit, newx = X, s = c(glm_lmbda)))  # make predictions
+  return (list(b = b, ypred = ypred))
+}
+
+ridge_glmnet_cv = function(y, X) {
+  lambdas = 10^seq(3, -2, by = -.1)
+  cv_fit = cv.glmnet(X, y, alpha = 0, lambda = lambdas)
+  opt_lambda = cv_fit$lambda.min
+  fit = cv_fit$glmnet.fit
+  b = coef(fit, s = opt_lambda)
+  ypred = c(predict(fit, newx = X, s = c(opt_lambda)))
+  return (list(fit=fit, ypred = ypred, opt_lambda=opt_lambda))
+}
+
+rsquare = function(ytrue, ypred) {
+  sse = sum((ypred - ytrue)^2)
+  sst = sum((ytrue - mean(ytrue))^2)
+  rsq = 1 - sse / sst
+  return (rsq)
+}
+
+n = 50
+p = 100
+sd = 2
+data = ridge_data(n, p, sd)
+lmbda = sd * sd
+r_ols = ridge_ols(data$y, data$X, lmbda)
+r_glmnet = ridge_glmnet(data$y, data$X, lmbda)
+r_glmnet_cv = ridge_glmnet_cv(data$y, data$X)
+
+rsquare(data$y, r_ols$ypred)
+rsquare(data$y, r_glmnet$ypred)
+rsquare(data$y, r_glmnet_cv$ypred)
